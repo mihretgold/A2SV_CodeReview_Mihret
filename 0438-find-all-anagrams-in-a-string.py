aa3c77538426d55ea81bd8ffe15d405dba2c8d63
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-08-02 10:28:34 +0300

    Update 0438-find-all-anagrams-in-a-string.py

--- 0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string.py
@@ -5,19 +5,26 @@ class Solution:
         
         length = len(s)
         start = 0
+
+        # Count the occurrences of characters in string p .
         for i in range(window):
             p_dict[p[i]] += 1
             
         answer = [] 
         end = 0
         s_dict = defaultdict(int)
+
         while end < length:
+            # Slide the window to the right until its size matches the length of p
             while end - start < window and end < length:
                 s_dict[s[end]] += 1
                 end += 1
             
+            # If the characters in the window form an anagram of p, record the starting index
             if s_dict == p_dict:
                 answer.append(start)
+                
+            # Move the window one step to the right by updating s_dict and start
             if s_dict[s[start]] == 1:
                 s_dict.pop(s[start])
             elif s_dict[s[start]] > 0:
